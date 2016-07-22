7fa16dc645d039e1df05ab480a6d5bc722c4c343
modhunter <pemodhunter@gmail.com>
2016-07-21 22:37:31 -0400

    correcting mis-termination of NeP.Engine.FaceRoll() function and re-indenting the rest of the file

--- facerool.lua
+++ facerool.lua
@@ -56,116 +56,117 @@ function NeP.Engine.FaceRoll()
 			end
 		end
 	end)
+end
 
-	-- cast on ground
-	function Engine.CastGround(spell, target)
-		showActiveSpell(spell)
+
+-- cast on ground
+function Engine.CastGround(spell, target)
+	showActiveSpell(spell)
+end
+
+-- Cast
+function Engine.Cast(spell, target)
+	showActiveSpell(spell)
+end
+
+-- Macro
+function Engine.Macro(text)
+end
+
+function Engine.UseItem(name, target)
+end
+
+function Engine.UseInvItem(slot)
+end
+
+function Engine.LineOfSight(a, b)
+	return true
+end
+
+-- Distance
+local rangeCheck = LibStub("LibRangeCheck-2.0")
+function Engine.Distance(a, b)
+	if UnitExists(b) then
+		local minRange, maxRange = rangeCheck:GetRange(b)
+		return maxRange or minRange
 	end
+	return 0
+end
 
-	-- Cast
-	function Engine.Cast(spell, target)
-		showActiveSpell(spell)
-	end
+-- Infront
+function Engine.Infront(a, b)
+	return true
+end
 
-	-- Macro
-	function Engine.Macro(text)
-	end
-
-	function Engine.UseItem(name, target)
-	end
-
-	function Engine.UseInvItem(slot)
-	end
-
-	function Engine.LineOfSight(a, b)
-		return true
-	end
-
-	-- Distance
-	local rangeCheck = LibStub("LibRangeCheck-2.0")
-	function Engine.Distance(a, b)
-		if UnitExists(b) then
-			local minRange, maxRange = rangeCheck:GetRange(b)
-			return maxRange or minRange
-		end
-		return 0
-	end
-
-	-- Infront
-	function Engine.Infront(a, b)
-		return true
-	end
-
-	--[[				Generic OM
-	---------------------------------------------------]]
-	local function GenericFilter(unit, objectDis)
-		if UnitExists(unit) then
-			local objectName = UnitName(unit)
-			local alreadyExists = false
-			-- Friendly Filter
-			if UnitCanAttack('player', unit) then
-				for i=1, #NeP.TempOM.unitEnemie do
-					local object = NeP.TempOM.unitEnemie[i]
-					if object.distance == objectDis and object.name == objectName then
-						alreadyExists = true
-					end
+--[[				Generic OM
+---------------------------------------------------]]
+local function GenericFilter(unit, objectDis)
+	if UnitExists(unit) then
+		local objectName = UnitName(unit)
+		local alreadyExists = false
+		-- Friendly Filter
+		if UnitCanAttack('player', unit) then
+			for i=1, #NeP.TempOM.unitEnemie do
+				local object = NeP.TempOM.unitEnemie[i]
+				if object.distance == objectDis and object.name == objectName then
+					alreadyExists = true
 				end
+			end
 			-- Enemie Filter
-			elseif UnitIsFriend('player', unit) then
-				for i=1, #NeP.TempOM.unitFriend do
-					local object = NeP.TempOM.unitFriend[i]
-					if object.distance == objectDis and object.name == objectName then
-						alreadyExists = true
-					end
+		elseif UnitIsFriend('player', unit) then
+			for i=1, #NeP.TempOM.unitFriend do
+				local object = NeP.TempOM.unitFriend[i]
+				if object.distance == objectDis and object.name == objectName then
+					alreadyExists = true
 				end
 			end
-			if not alreadyExists then return true end
 		end
-		return false
+		if not alreadyExists then return true end
 	end
+	return false
+end
 
-	function NeP.OM.Maker()
-		-- Self
-		NeP.OM.addToOM('player', 5)
-		-- Mouseover
-		if UnitExists('mouseover') then
-			local object = 'mouseover'
-			local ObjDistance = Engine.Distance('player', object)
-			if GenericFilter(object, ObjDistance) then
-				if ObjDistance <= 100 then
-					NeP.OM.addToOM(object)
-				end
+function NeP.OM.Maker()
+	-- Self
+	NeP.OM.addToOM('player', 5)
+	-- Mouseover
+	if UnitExists('mouseover') then
+		local object = 'mouseover'
+		local ObjDistance = Engine.Distance('player', object)
+		if GenericFilter(object, ObjDistance) then
+			if ObjDistance <= 100 then
+				NeP.OM.addToOM(object)
 			end
 		end
-		-- Target Cache
-		if UnitExists('target') then
-			local object = 'target'
-			local ObjDistance = Engine.Distance('player', object)
-			if GenericFilter(object, ObjDistance) then
-				if ObjDistance <= 100 then
-					NeP.OM.addToOM(object)
-				end
+	end
+	-- Target Cache
+	if UnitExists('target') then
+		local object = 'target'
+		local ObjDistance = Engine.Distance('player', object)
+		if GenericFilter(object, ObjDistance) then
+			if ObjDistance <= 100 then
+				NeP.OM.addToOM(object)
 			end
 		end
-		-- If in Group scan frames...
-		if IsInGroup() or IsInRaid() then
-			local prefix = (IsInRaid() and 'raid') or 'party'
-			for i = 1, GetNumGroupMembers() do
-				-- Enemie
-				local target = prefix..i..'target'
-				local ObjDistance = Engine.Distance('player', target)
-				if GenericFilter(target, ObjDistance) then
-					if ObjDistance <= 100 then
-						NeP.OM.addToOM(target)
-					end
+	end
+	-- If in Group scan frames...
+	if IsInGroup() or IsInRaid() then
+		local prefix = (IsInRaid() and 'raid') or 'party'
+		for i = 1, GetNumGroupMembers() do
+			-- Enemie
+			local target = prefix..i..'target'
+			local ObjDistance = Engine.Distance('player', target)
+			if GenericFilter(target, ObjDistance) then
+				if ObjDistance <= 100 then
+					NeP.OM.addToOM(target)
 				end
-				-- Friendly
-				local friendly = prefix..i
-				local ObjDistance = Engine.Distance('player', friendly)
-				if GenericFilter(friendly, ObjDistance) then
-					if ObjDistance <= 100 then
-						NeP.OM.addToOM(friendly)
-					end
+			end
+			-- Friendly
+			local friendly = prefix..i
+			local ObjDistance = Engine.Distance('player', friendly)
+			if GenericFilter(friendly, ObjDistance) then
+				if ObjDistance <= 100 then
+					NeP.OM.addToOM(friendly)
 				end
 			end
 		end
