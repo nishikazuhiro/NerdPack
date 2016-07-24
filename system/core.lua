1b12f1b2474072843a76cdee5d1202b13b036749
Jaime Marques <mrthesoulz@gmail.com>
2016-07-24 14:57:10 +0100

    Version Bump

--- system/core.lua
+++ system/core.lua
@@ -3,8 +3,8 @@ NeP = {
 		Name = 'NerdPack',
 		Nick = 'NeP',
 		Author = 'MrTheSoulz',
-		Version = '7.0.3.2',
-		Branch = 'Live',
+		Version = '7.0.3.3',
+		Branch = 'LIVE',
 	},
 	Interface = {
 		Logo = 'Interface\\AddOns\\NerdPack\\media\\logo.blp',
