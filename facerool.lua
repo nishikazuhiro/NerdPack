1a7587f87a644a27f3b3e39be7fc895a3d4ceebf
modhunter <pemodhunter@gmail.com>
2016-07-26 12:53:55 -0400

    switching GenericFilter to use GUID instead of unit name & distance

--- facerool.lua
+++ facerool.lua
@@ -132,7 +132,7 @@ function NeP.Engine.FaceRoll()
 		if UnitExists('mouseover') then
 			local object = 'mouseover'
 			local ObjDistance = Engine.Distance('player', object)
-			if GenericFilter(object, ObjDistance) then
+			if GenericFilter(object) then
 				if ObjDistance <= 100 then
 					NeP.OM.addToOM(object)
 				end
@@ -142,7 +142,7 @@ function NeP.Engine.FaceRoll()
 		if UnitExists('target') then
 			local object = 'target'
 			local ObjDistance = Engine.Distance('player', object)
-			if GenericFilter(object, ObjDistance) then
+			if GenericFilter(object) then
 				if ObjDistance <= 100 then
 					NeP.OM.addToOM(object)
 				end
@@ -155,7 +155,7 @@ function NeP.Engine.FaceRoll()
 				-- Enemie
 				local target = prefix..i..'target'
 				local ObjDistance = Engine.Distance('player', target)
-				if GenericFilter(target, ObjDistance) then
+				if GenericFilter(target) then
 					if ObjDistance <= 100 then
 						NeP.OM.addToOM(target)
 					end
@@ -163,7 +163,7 @@ function NeP.Engine.FaceRoll()
 				-- Friendly
 				local friendly = prefix..i
 				local ObjDistance = Engine.Distance('player', friendly)
-				if GenericFilter(friendly, ObjDistance) then
+				if GenericFilter(friendly) then
 					if ObjDistance <= 100 then
 						NeP.OM.addToOM(friendly)
 					end
