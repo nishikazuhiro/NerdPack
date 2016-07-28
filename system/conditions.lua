1697362738b66be00b829fb2b699d3c3a53ca87b
Jaime Marques <mrthesoulz@gmail.com>
2016-07-29 00:09:51 +0100

    add .maelstrom

--- system/conditions.lua
+++ system/conditions.lua
@@ -427,12 +427,13 @@ NeP.DSL.RegisterConditon("movingfor", function(target)
 	end
 end)
 
--- DK Power
+NeP.DSL.RegisterConditon("maelstrom", function(target, spell)
+    return UnitPower(target, SPELL_POWER_MAELSTROM)
+end)
 
 NeP.DSL.RegisterConditon("runicpower", function(target, spell)
 	return UnitPower(target, SPELL_POWER_RUNIC_POWER)
 end)
-
 
 NeP.DSL.RegisterConditon("runes.count", function(target)
 	local count = 0
