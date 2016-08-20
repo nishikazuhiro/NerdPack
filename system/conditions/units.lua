f41b4343d857a8a4d35e28c74426409490de9605
Jaime Marques <mrthesoulz@gmail.com>
2016-08-20 17:35:55 +0100

    add dummy check for area.enemies

--- system/conditions/units.lua
+++ system/conditions/units.lua
@@ -362,7 +362,7 @@ RegisterConditon("area.enemies", function(unit, distance)
 	if UnitExists(unit) then
 		for i=1, #NeP.OM.unitEnemie do
 			local Obj = NeP.OM.unitEnemie[i]
-			if UnitAffectingCombat(Obj.key)
+			if (UnitAffectingCombat(Obj.key) or isDummy(Obj.key))
 			and UnitExists(Obj.key)
 			and (NeP.Engine.Distance(unit, Obj.key) <= distance) then
 				total = total +1
