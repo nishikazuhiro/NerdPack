0a109952bd6c0179844abf268727d44d10ce95fc
Jaime Marques <mrthesoulz@gmail.com>
2016-08-23 03:16:59 +0100

    add states

---
+++ system/tooltip.lua
@@ -0,0 +1,44 @@
+NeP.Tooltip = {
+	frame = CreateFrame('GameTooltip', 'PE_ScanningTooltip', UIParent, 'GameTooltipTemplate')
+}
+
+function NeP.Tooltip.Scan_Buff(target, pattern)
+	for i = 1, 40 do
+		NeP.Tooltip.frame:SetOwner(UIParent, 'ANCHOR_NONE')
+		NeP.Tooltip.frame:SetUnitBuff(target, i)
+		local tooltipText = _G["PE_ScanningTooltipTextLeft2"]:GetText()
+		if tooltipText then
+			if type(pattern) == 'string' then
+				local match = tooltipText:lower():match(pattern)
+				if match then return true end
+			elseif type(pattern) == 'table' then
+				for _, curPattern in pairs(pattern) do
+					local match = tooltipText:lower():match(curPattern)
+					if match then return true end
+				end
+			end
+		end
+	end
+	return false
+end
+
+NeP.Tooltip.Scan_Debuff = function(target, pattern)
+	-- debuffs
+	for i = 1, 40 do
+		NeP.Tooltip.frame:SetOwner(UIParent, 'ANCHOR_NONE')
+		NeP.Tooltip.frame:SetUnitDebuff(target, i)
+		local tooltipText = _G["PE_ScanningTooltipTextLeft2"]:GetText()
+		if tooltipText then
+			if type(pattern) == 'string' then
+				local match = tooltipText:lower():match(pattern)
+				if match then return true end
+			elseif type(pattern) == 'table' then
+				for _, curPattern in pairs(pattern) do
+					local match = tooltipText:lower():match(curPattern)
+					if match then return true end
+				end
+			end
+		end
+	end
+	return false
+end
