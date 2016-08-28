7e93e516a4e5bba7d257a3970989375f85c14e0d
Jaime Marques <mrthesoulz@gmail.com>
2016-08-28 04:32:40 +0100

    fix buffs debuffs

--- system/APIs.lua
+++ system/APIs.lua
@@ -5,8 +5,9 @@ local APIs = NeP.APIs
 APIs['UnitBuff'] = function(target, spell, owner)
 	local buff, count, caster, expires, spellID
 	if tonumber(spell) then
-		local go = true
+		local go, i = true, 0
 		repeat
+			i = i + 1
 			buff,_,_,count,_,_,expires,caster,_,_,spellID = _G['UnitBuff'](target, i)
 			if not owner then
 				if spellID == tonumber(spell) and (caster == 'player' or caster == 'pet') then
@@ -35,8 +36,9 @@ end
 APIs['UnitDebuff'] = function(target, spell, owner)
 	local debuff, count, caster, expires, spellID, power
 	if tonumber(spell) then
-		local go = true
+		local go, i = true, 0
 		repeat
+			i = i + 1
 			debuff,_,_,count,_,_,expires,caster,_,_,spellID,_,_,_,power = _G['UnitDebuff'](target, i)
 			if not owner then
 				if spellID == tonumber(spell) and (caster == 'player' or caster == 'pet') then
