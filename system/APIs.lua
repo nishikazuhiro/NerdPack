b999766d763c9f02012cb3692b82823bc4c09f3e
Jaime Marques <mrthesoulz@gmail.com>
2016-08-28 19:41:01 +0100

    improve reaction filter

--- system/APIs.lua
+++ system/APIs.lua
@@ -2,13 +2,28 @@ NeP.APIs = {}
 
 local APIs = NeP.APIs
 
+local function rFilter(expires, duration)
+	if expires then
+		local reactionTime = GetReactionTime()
+		expires = expires - reactionTime
+		-- Break if debuff is gonna end
+		if expires <= reactionTime then
+			return false
+		-- Break if faster then we can react
+		elseif expires > (duration-reactionTime) then
+			return false
+		end
+	end
+	return true
+end
+
 APIs['UnitBuff'] = function(target, spell, owner)
 	local buff, count, caster, expires, spellID
 	if tonumber(spell) then
 		local go, i = true, 0
 		while i <= 40 and go do
 			i = i + 1
-			buff,_,_,count,_,_,expires,caster,_,_,spellID = _G['UnitBuff'](target, i)
+			buff,_,_,count,_,duration,expires,caster,_,_,spellID = _G['UnitBuff'](target, i)
 			if not owner then
 				if spellID == tonumber(spell) and (caster == 'player' or caster == 'pet') then
 					go = false
@@ -20,17 +35,12 @@ APIs['UnitBuff'] = function(target, spell, owner)
 			end
 		end
 	else
-		buff,_,_,count,_,_,expires,caster = _G['UnitBuff'](target, spell)
+		buff,_,_,count,_,duration,expires,caster = _G['UnitBuff'](target, spell)
 	end
 	-- This adds some random factor
-	if expires then
-		local reactionTime = GetReactionTime()
-		expires = expires - reactionTime
-		if expires <= 0 then
-			return
-		end
+	if rFilter(expires, duration) then
+		return buff, count, expires, caster
 	end
-	return buff, count, expires, caster
 end
 
 APIs['UnitDebuff'] = function(target, spell, owner)
@@ -39,7 +49,7 @@ APIs['UnitDebuff'] = function(target, spell, owner)
 		local go, i = true, 0
 		while i <= 40 and go do
 			i = i + 1
-			debuff,_,_,count,_,_,expires,caster,_,_,spellID,_,_,_,power = _G['UnitDebuff'](target, i)
+			debuff,_,_,count,_,duration,expires,caster,_,_,spellID,_,_,_,power = _G['UnitDebuff'](target, i)
 			if not owner then
 				if spellID == tonumber(spell) and (caster == 'player' or caster == 'pet') then
 					go = false
@@ -51,15 +61,10 @@ APIs['UnitDebuff'] = function(target, spell, owner)
 			end
 		end
 	else
-		debuff,_,_,count,_,_,expires,caster = _G['UnitDebuff'](target, spell)
+		debuff,_,_,count,_,duration,expires,caster = _G['UnitDebuff'](target, spell)
 	end
 	-- This adds some random factor
-	if expires then
-		local reactionTime = GetReactionTime()
-		expires = expires - reactionTime
-		if expires <= 0 then
-			return
-		end
+	if rFilter(expires, duration) then
+		return debuff, count, expires, caster, power
 	end
-	return debuff, count, expires, caster, power
 end
