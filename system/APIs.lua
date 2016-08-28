e6c5760810ae20a026bc1c58251dc0418890a761
Jaime Marques <mrthesoulz@gmail.com>
2016-08-28 19:56:00 +0100

    clean

--- system/APIs.lua
+++ system/APIs.lua
@@ -17,54 +17,51 @@ local function rFilter(expires, duration)
 	return true
 end
 
+local function oFilter(owner, spell, spellID, caster)
+	if not owner then
+		if spellID == tonumber(spell) and (caster == 'player' or caster == 'pet') then
+			return false
+		end
+	elseif owner == "any" then
+		if spellID == tonumber(spell) then
+			return false
+		end
+	end
+	return true
+end
+
 APIs['UnitBuff'] = function(target, spell, owner)
-	local buff, count, caster, expires, spellID
+	local name, count, caster, expires, spellID
 	if tonumber(spell) then
 		local go, i = true, 0
 		while i <= 40 and go do
 			i = i + 1
-			buff,_,_,count,_,duration,expires,caster,_,_,spellID = _G['UnitBuff'](target, i)
-			if not owner then
-				if spellID == tonumber(spell) and (caster == 'player' or caster == 'pet') then
-					go = false
-				end
-			elseif owner == "any" then
-				if spellID == tonumber(spell) then
-					go = false
-				end
-			end
+			name,_,_,count,_,duration,expires,caster,_,_,spellID = _G['UnitBuff'](target, i)
+			go = oFilter(owner, spell, spellID, caster)
 		end
 	else
-		buff,_,_,count,_,duration,expires,caster = _G['UnitBuff'](target, spell)
+		name,_,_,count,_,duration,expires,caster = _G['UnitBuff'](target, spell)
 	end
 	-- This adds some random factor
-	if rFilter(expires, duration) then
-		return buff, count, expires, caster
+	if name and rFilter(expires, duration) then
+		return name, count, expires, caster
 	end
 end
 
 APIs['UnitDebuff'] = function(target, spell, owner)
-	local debuff, count, caster, expires, spellID, power
+	local name, count, caster, expires, spellID, power
 	if tonumber(spell) then
 		local go, i = true, 0
 		while i <= 40 and go do
 			i = i + 1
-			debuff,_,_,count,_,duration,expires,caster,_,_,spellID,_,_,_,power = _G['UnitDebuff'](target, i)
-			if not owner then
-				if spellID == tonumber(spell) and (caster == 'player' or caster == 'pet') then
-					go = false
-				end
-			elseif owner == "any" then
-				if spellID == tonumber(spell) then
-					go = false
-				end
-			end
+			name,_,_,count,_,duration,expires,caster,_,_,spellID,_,_,_,power = _G['UnitDebuff'](target, i)
+			go = oFilter(owner, spell, spellID, caster)
 		end
 	else
-		debuff,_,_,count,_,duration,expires,caster = _G['UnitDebuff'](target, spell)
+		name,_,_,count,_,duration,expires,caster = _G['UnitDebuff'](target, spell)
 	end
 	-- This adds some random factor
-	if rFilter(expires, duration) then
-		return debuff, count, expires, caster, power
+	if name and rFilter(expires, duration) then
+		return name, count, expires, caster, power
 	end
 end
