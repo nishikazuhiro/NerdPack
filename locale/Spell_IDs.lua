4c98ba9f6ae5e7bcd7e6121b5e576f32fb530848
Jaime Marques <mrthesoulz@gmail.com>
2016-09-15 03:34:34 +0100

    paladin IDs

--- locale/Spell_IDs.lua
+++ locale/Spell_IDs.lua
@@ -10,7 +10,17 @@ local SpellID = {
 	["Trash"] = 106830,
 	["Swipe"] = 213764,
 	["Tiger's Fury"] = 5217,
-	["Prowl"] = 5215
+	["Prowl"] = 5215,
+	-- Paladin
+	["Hand of Reckoning"] = 62124,
+	["Arcane Blast"] = 32935,
+	["Eye of Tyr"] = 209202,
+	["Hammer of the Righteous"] = 53595,
+	["Judgment"] = 20271,
+	["Seraphim"] = 152262,
+	["Shield of the Righteous"] = 53600,
+	["Consecration"] = 26573,
+	["Avenging Wrath"] = 31884,
 }
 
 local GetSpellInfo = GetSpellInfo
