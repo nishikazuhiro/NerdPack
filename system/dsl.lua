73e67a6541a7308bbb80317dda22a0386e5cb3a5
Jaime Marques <mrthesoulz@gmail.com>
2016-09-16 03:38:01 +0100

    OOOPS!

--- system/dsl.lua
+++ system/dsl.lua
@@ -4,15 +4,15 @@ NeP.DSL = {
 
 local DSL = NeP.DSL
 
-local function ExecuteMath = {
-	['>='] = function(arg1, arg2) return (arg1 >= arg2) end,
-	['<='] = function(arg1, arg2) return (arg1 <= arg2) end,
-	['=='] = function(arg1, arg2) return (arg1 == arg2) end,
-	['~='] = function(arg1, arg2) return (arg1 ~= arg2) end,
-	['+'] = function(arg1, arg2) return (arg1 + arg2) end,
-	['-'] = function(arg1, arg2) return (arg1 - arg2) end,
-	['/'] = function(arg1, arg2) return (arg1 / arg2) end,
-	['*'] = function(arg1, arg2) return (arg1 * arg2) end
+local ExecuteMath = {
+	['>='] = function(arg1, arg2) return arg1 >= arg2 end,
+	['<='] = function(arg1, arg2) return arg1 <= arg2 end,
+	['=='] = function(arg1, arg2) return arg1 == arg2 end,
+	['~='] = function(arg1, arg2) return arg1 ~= arg2 end,
+	['+'] = function(arg1, arg2) return arg1 + arg2 end,
+	['-'] = function(arg1, arg2) return arg1 - arg2 end,
+	['/'] = function(arg1, arg2) return arg1 / arg2 end,
+	['*'] = function(arg1, arg2) return arg1 * arg2 end
 }
 
 local OPs = '[><=!~%+%-%*%/]'
