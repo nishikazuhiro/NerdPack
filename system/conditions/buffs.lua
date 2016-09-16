ef9d60faf7c04dbb66065eb37f299124b94a7fe6
Jaime Marques <mrthesoulz@gmail.com>
2016-09-16 01:18:13 +0100

    clean

--- system/conditions/buffs.lua
+++ system/conditions/buffs.lua
@@ -9,14 +9,11 @@ local RegisterConditon = NeP.DSL.RegisterConditon
 ]]
 
 local heroismBuffs = { 32182, 90355, 80353, 2825, 146555 }
-
 RegisterConditon("hashero", function(target, spell)
 	for i = 1, #heroismBuffs do
 		local SpellName = GetSpellName(heroismBuffs[i])
 		local buff = NeP.APIs['UnitBuff']('player', SpellName, "any")
-		if buff then
-			return true
-		end
+		if buff then return true end
 	end
 	return false
 end)
@@ -25,31 +22,27 @@ end)
 ------------------------------------------------------------------------------------------
 RegisterConditon("buff", function(target, spell)
 	local buff,_,_,caster = NeP.APIs['UnitBuff'](target, spell)
-	if not not buff and (caster == 'player' or caster == 'pet') then
-		return true
-	end
-	return false
+	return buff and (caster == 'player' or caster == 'pet')
 end)
 
 RegisterConditon("buff.any", function(target, spell)
 	local buff,_,_,caster = NeP.APIs['UnitBuff'](target, spell, "any")
-	if not not buff then
-		return true
-	end
-	return false
+	return buff
 end)
 
 RegisterConditon("buff.count", function(target, spell)
 	local buff,count,_,caster = NeP.APIs['UnitBuff'](target, spell)
-		if not not buff and (caster == 'player' or caster == 'pet') then
+	if buff and (caster == 'player' or caster == 'pet') then
 		return count
 	end
 	return 0
 end)
 
 RegisterConditon("buff.duration", function(target, spell)
+	print('hit')
 	local buff,_,expires,caster = NeP.APIs['UnitBuff'](target, spell)
-	if not not buff and (caster == 'player' or caster == 'pet') then
+	if buff and (caster == 'player' or caster == 'pet') then
+		print((expires - GetTime()))
 		return (expires - GetTime())
 	end
 	return 0
@@ -60,23 +53,17 @@ end)
 
 RegisterConditon("debuff", function(target, spell)
 	local debuff,_,_,caster = NeP.APIs['UnitDebuff'](target, spell)
-	if not not debuff and (caster == 'player' or caster == 'pet') then
-		return true
-	end
-	return false
+	return debuff and (caster == 'player' or caster == 'pet')
 end)
 
 RegisterConditon("debuff.any", function(target, spell)
 	local debuff,_,_,caster = NeP.APIs['UnitDebuff'](target, spell, "any")
-	if not not debuff then
-		return true
-	end
-	return false
+	return debuff
 end)
 
 RegisterConditon("debuff.count", function(target, spell)
 	local debuff,count,_,caster = NeP.APIs['UnitDebuff'](target, spell)
-	if not not debuff and (caster == 'player' or caster == 'pet') then
+	if debuff and (caster == 'player' or caster == 'pet') then
 		return count
 	end
 	return 0
@@ -84,7 +71,7 @@ end)
 
 RegisterConditon("debuff.duration", function(target, spell)
 	local debuff,_,expires,caster = NeP.APIs['UnitDebuff'](target, spell)
-	if not not debuff and (caster == 'player' or caster == 'pet') then
+	if debuff and (caster == 'player' or caster == 'pet') then
 		return (expires - GetTime())
 	end
 	return 0
