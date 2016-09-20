67348fb44f42ff00d690629a0817bf81d34574be
Jaime Marques <mrthesoulz@gmail.com>
2016-09-20 07:52:32 +0100

    Oops

--- system/tooltip.lua
+++ system/tooltip.lua
@@ -6,7 +6,7 @@ function NeP.Tooltip.Scan_Buff(target, pattern)
 	for i = 1, 40 do
 		NeP.Tooltip.frame:SetOwner(UIParent, 'ANCHOR_NONE')
 		NeP.Tooltip.frame:SetUnitBuff(target, i)
-		local tooltipText = _G["NeP_ScanningTooltip"]:GetText()
+		local tooltipText = _G["NeP_ScanningTooltipTextLeft2"]:GetText()
 		if tooltipText then
 			if type(pattern) == 'string' then
 				local match = tooltipText:lower():match(pattern)
@@ -27,7 +27,7 @@ function NeP.Tooltip.Scan_Debuff(target, pattern)
 	for i = 1, 40 do
 		NeP.Tooltip.frame:SetOwner(UIParent, 'ANCHOR_NONE')
 		NeP.Tooltip.frame:SetUnitDebuff(target, i)
-		local tooltipText = _G["NeP_ScanningTooltip"]:GetText()
+		local tooltipText = _G["NeP_ScanningTooltipTextLeft2"]:GetText()
 		if tooltipText then
 			if type(pattern) == 'string' then
 				local match = tooltipText:lower():match(pattern)
@@ -41,4 +41,8 @@ function NeP.Tooltip.Scan_Debuff(target, pattern)
 		end
 	end
 	return false
+end
+
+function NeP.Tooltip.CastTime(target, pattern)
+	--WIP
 end
