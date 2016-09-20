f0bd09a050c2ba4807ebca6f51612fdc6249c0d1
Jaime Marques <mrthesoulz@gmail.com>
2016-09-20 06:16:13 +0100

    sanity check

--- system/timer.lua
+++ system/timer.lua
@@ -10,21 +10,22 @@ C_Timer.NewTicker(0.1, (function()
 	for i=1, #timers do
 		local timer = timers[i]
 		if timer.lastCall < time then
-			debug('timer', 'Timer Fire: ' .. timer.name)
-			timer.callback()
+			--debug('timer', 'Timer Fire: ' .. timer.name)
+            timer.callback()
 			timer.lastCall = (time + timer.time)
         end
 	end
 end), nil)
 
-function timer.Sync(_name, time, _callback, _prio)
-	debug('timer', 'Timer Registered: ' .. _name)
+function timer.Sync(name, time, callback, prio)
+    if type(callback) ~= 'function' then return end
+	--debug('timer', 'Timer Registered: ' .. name)
 	timers[#timers+1] = {
-		name = _name,
-		callback = _callback,
+		name = name,
+		callback = callback,
 		time = time,
 		lastCall = 0,
-		prio = _prio or 2
+		prio = prio or 2
 	}
 	table.sort(timers, function(a,b) return a.prio < b.prio end)
 end
