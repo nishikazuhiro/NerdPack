25f4a80495c5c09e4f2773a2493bbaf0cdf8480c
Jaime Marques <mrthesoulz@gmail.com>
2016-09-30 15:58:52 +0100

    clean

--- system/tooltip.lua
+++ system/tooltip.lua
@@ -2,43 +2,34 @@ NeP.Tooltip = {
 	frame = CreateFrame('GameTooltip', 'NeP_ScanningTooltip', UIParent, 'GameTooltipTemplate')
 }
 
+local function pPattern(pattern)
+	if type(pattern) == 'string' then
+		local match = tooltipText:lower():match(pattern)
+		if match then return true end
+	elseif type(pattern) == 'table' then
+		for i=1, #pattern do
+			local match = tooltipText:lower():match(pattern[i])
+			if match then return true end
+		end
+	end
+end
+
 function NeP.Tooltip.Scan_Buff(target, pattern)
 	for i = 1, 40 do
 		NeP.Tooltip.frame:SetOwner(UIParent, 'ANCHOR_NONE')
 		NeP.Tooltip.frame:SetUnitBuff(target, i)
 		local tooltipText = _G["NeP_ScanningTooltipTextLeft2"]:GetText()
-		if tooltipText then
-			if type(pattern) == 'string' then
-				local match = tooltipText:lower():match(pattern)
-				if match then return true end
-			elseif type(pattern) == 'table' then
-				for _, curPattern in pairs(pattern) do
-					local match = tooltipText:lower():match(curPattern)
-					if match then return true end
-				end
-			end
-		end
+		return tooltipText and pPattern(pattern)
 	end
 	return false
 end
 
 function NeP.Tooltip.Scan_Debuff(target, pattern)
-	-- debuffs
 	for i = 1, 40 do
 		NeP.Tooltip.frame:SetOwner(UIParent, 'ANCHOR_NONE')
 		NeP.Tooltip.frame:SetUnitDebuff(target, i)
 		local tooltipText = _G["NeP_ScanningTooltipTextLeft2"]:GetText()
-		if tooltipText then
-			if type(pattern) == 'string' then
-				local match = tooltipText:lower():match(pattern)
-				if match then return true end
-			elseif type(pattern) == 'table' then
-				for _, curPattern in pairs(pattern) do
-					local match = tooltipText:lower():match(curPattern)
-					if match then return true end
-				end
-			end
-		end
+		return tooltipText and pPattern(pattern)
 	end
 	return false
 end
@@ -47,16 +38,5 @@ function NeP.Tooltip.Unit(target, pattern)
 	NeP.Tooltip.frame:SetOwner(UIParent, 'ANCHOR_NONE')
 	NeP.Tooltip.frame:SetUnit(target)
 	local tooltipText = _G["NeP_ScanningTooltipTextLeft2"]:GetText()
-	if tooltipText then
-		if type(pattern) == 'string' then
-			local match = tooltipText:lower():match(pattern)
-			if match then return true end
-		elseif type(pattern) == 'table' then
-			for i=1, #pattern do
-				local match = tooltipText:lower():match(pattern[i])
-				if match then return true end
-			end
-		end
-	end
-	return false
+	return tooltipText and pPattern(pattern)
 end
