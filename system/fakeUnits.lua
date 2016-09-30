3c60737ef7f8d0d5c8d1a795513d87cfc36360f8
Jaime Marques <mrthesoulz@gmail.com>
2016-09-30 14:36:35 +0100

    fix fakeunits

--- system/fakeUnits.lua
+++ system/fakeUnits.lua
@@ -20,10 +20,9 @@ function NeP.FakeUnits.Filter(unit)
 	for token,func in pairs(Units) do
 		if unit:find(token) then
 			local arg1, arg2 = unit:match('(.+)%((.+)%)')
-			if arg2 then unit = arg1 end
 			local num = unit:match("%d+") or 1
-			local real_unit = func(num, arg2)
-			return real_unit and unit:gsub(token, real_unit)
+			local real_unit = func(tonumber(num), arg2)
+			return real_unit
 		end
 	end
 	return unit
@@ -34,7 +33,7 @@ NeP.FakeUnits.Add('lowest', function(num, role)
 	local tempTable = {}
 	for i=1, #Healing.Units do
 		local Obj = Healing.Units[i]
-		if role and Obj.Role == role:upper() or not role then
+		if (role and Obj.role == role:upper()) or not role then
 			tempTable[#tempTable+1] = {
 				key = Obj.key,
 				prio = prio
