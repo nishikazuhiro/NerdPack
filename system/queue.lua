f92f0b1e6b76c6248be7c49640d5bb65178cdf5e
Jaime Marques <mrthesoulz@gmail.com>
2016-09-30 13:13:11 +0100

    Fix Cast_Queue

--- system/queue.lua
+++ system/queue.lua
@@ -41,7 +41,7 @@ NeP.Timer.Sync("nep_queue", 0.01, function()
 			else
 				local func, spell, target = Engine:Parse(unpack(eQueue[i]))
 				if func then
-					func(func, spell)
+					func(spell, spell)
 					table.remove(eQueue, i)
 				end
 			end
