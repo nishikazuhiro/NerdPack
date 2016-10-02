a3959039622ecd373e05f90a415d5e430f5c8980
Jaime Marques <mrthesoulz@gmail.com>
2016-10-02 21:34:23 +0100

    fix error in class table

--- Tables/class.lua
+++ Tables/class.lua
@@ -1,227 +1,227 @@
 NeP.ClassTable = {
-	1 = { -- Warrior
+	{ -- Warrior
 		Color_HEX = 'c79c6e',
 		Color_RGB = {0.78,0.61,0.43},
-		71 = {
+		[71] = {
 			range = 'melee',
 			name = 'Arms',
 			role = 'DPS'
 		},
-		72 = {
+		[72] = {
 			range = 'melee',
 			name = 'Fury',
 			role = 'DPS'
 		},
-		73 = {
+		[73] = {
 			range = 'melee',
 			name = 'Protection',
 			role = 'TANK'
 		}
 	},
-	2 = {  -- Paladin
+	{  -- Paladin
 		Color_HEX = 'f58cba',
 		Color_RGB = {0.96,0.55,0.73},
-		65 = {
+		[65] = {
 			range = 'melee',
 			name = 'Holy',
 			role = 'HEALER'
 		},
-		66 = {
+		[66] = {
 			range = 'melee',
 			name = 'Protection',
 			role = 'TANK'
 		},
-		70 = {
+		[70] = {
 			range = 'melee',
 			name = 'Retribution',
 			role = 'DPS'
 		}
 	},
-	3 = { -- Hunter
+	{ -- Hunter
 		Color_HEX = 'abd473',
 		Color_RGB = {0.67,0.83,0.45},
-		253 = {
+		[253] = {
 			range = 'melee',
 			name = 'Beast Mastery',
 			role = 'DPS'
 		},
-		254 = {
+		[254] = {
 			range = 'melee',
 			name = 'Marksmanship',
 			role = 'DPS'
 		},
-		255 = {
+		[255] = {
 			range = 'melee',
 			name = 'Survival',
 			role = 'DPS'
 		}
 	},
-	4 = { -- Rogue
+	{ -- Rogue
 		Color_HEX = 'fff569',
 		Color_RGB = {1,0.96,0.41},
-		259 = {
+		[259] = {
 			range = 'melee',
 			name = 'Assassination',
 			role = 'DPS'
 		},
-		260 = {
+		[260] = {
 			range = 'melee',
 			name = 'Outlaw',
 			role = 'DPS'
 		},
-		261 = {
+		[261] = {
 			range = 'melee',
 			name = 'Subtlety',
 			role = 'DPS'
 		}
 	},
-	5 = {  -- Priest
+	{  -- Priest
 		Color_HEX = 'ffffff',
 		Color_RGB = {1,1,1},
-		256 = {
+		[256] = {
 			range = 'Ranged',
 			name = 'Discipline',
 			role = 'HEALER'
 		},
-		257 = {
+		[257] = {
 			range = 'Ranged',
 			name = 'Holy',
 			role = 'HEALER'
 		},
-		258 = {
+		[258] = {
 			range = 'Ranged',
 			name = 'Shadow',
 			role = 'HEALER'
 		}
 	},
-	6 = { -- DeathKnight
+	{ -- DeathKnight
 		Color_HEX = 'c41f3b',
 		Color_RGB = {0.77,0.12,0.23},
-		250 = {
+		[250] = {
 			range = 'melee',
 			name = 'Blood',
 			role = 'TANK'
 		},
-		251 = {
+		[251] = {
 			range = 'melee',
 			name = 'Frost',
 			role = 'DPS'
 		},
-		252 = {
+		[252] = {
 			range = 'melee',
 			name = 'Unholy',
 			role = 'DPS'
 		}
 	},
-	7 = {  -- Shaman
+	{  -- Shaman
 		Color_HEX = '0070de',
 		Color_RGB = {0,0.44,0.87},
-		262 = {
+		[262] = {
 			range = 'Ranged',
 			name = 'Elemental',
 			role = 'DPS'
 		},
-		263 = {
+		[263] = {
 			range = 'melee',
 			name = 'Enhancement',
 			role = 'DPS'
 		},
-		264 = {
+		[264] = {
 			range = 'Ranged',
 			name = 'Restoration',
 			role = 'HEALER'
 		}
 	},
-	8 = {  -- Mage
+	{  -- Mage
 		Color_HEX = '69ccf0',
 		Color_RGB = {0.41,0.8,0.94},
-		62 = {
+		[62] = {
 			range = 'Ranged',
 			name = 'Arcane',
 			role = 'DPS'
 		},
-		63 = {
+		[63] = {
 			range = 'Ranged',
 			name = 'Fire',
 			role = 'DPS'
 		},
-		64 = {
+		[64] = {
 			range = 'Ranged',
 			name = 'Frost',
 			role = 'DPS'
 		}
 	},
-	9 = { -- Warlock
+	{ -- Warlock
 		Color_HEX = '9482c9',
 		Color_RGB = {0.58,0.51,0.79},
-		265 = {
+		[265] = {
 			range = 'Ranged',
 			name = 'Affliction',
 			role = 'DPS'
 		},
-		266 = {
+		[266] = {
 			range = 'Ranged',
 			name = 'Demonology',
 			role = 'DPS'
 		},
-		267 = {
+		[267] = {
 			range = 'Ranged',
 			name = 'Destruction',
 			role = 'DPS'
 		}
 	},
-	10 = { -- Monk
+	{ -- Monk
 		Color_HEX = '00ff96',
 		Color_RGB = {r = 0,1,0.59},
-		268 = {
+		[268] = {
 			range = 'melee',
 			name = 'Brewmaster',
 			role = 'TANK'
 		},
-		269 = {
+		[269] = {
 			range = 'melee',
 			name = 'Windwalker',
 			role = 'DPS'
 		},
-		270 = {
+		[270] = {
 			range = 'Ranged',
 			name = 'Mistweaver',
 			role = 'HEALER'
 		}
 	},
-	11 = { -- Druid
+	{ -- Druid
 		Color_HEX = 'ff7d0a',
 		Color_RGB = {1,0.49,0.04},
-		102 = {
+		[102] = {
 			range = 'Ranged',
 			name = 'Balance',
 			role = 'DPS'
 		},
-		103 = {
+		[103] = {
 			range = 'melee',
 			name = 'Feral Combat',
 			role = 'DPS'
 		},
-		104 = {
+		[104] = {
 			range = 'melee',
 			name = 'Guardian',
 			role = 'TANK'
 		},
-		105 = {
+		[105] = {
 			range = 'Ranged',
 			name = 'Restoration',
 			role = 'HEALER'
 		}
 	},
-	12 = { -- Demon Hunter
-		Color_HEX = 'A330C9'
+	{ -- Demon Hunter
+		Color_HEX = 'A330C9',
 		Color_RGB = {0.64,0.19,0.79},
-		577 = {
+		[577] = {
 			range = 'melee',
 			name = 'Havoc',
 			role = 'DPS'
 		},
-		581 = {
+		[581] = {
 			range = 'melee',
 			name = 'Vengeance',
 			role = 'TANK'
