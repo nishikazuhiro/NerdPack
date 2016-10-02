17b1f6c15c3cc8f70f2cb47a2090e19626b490dd
Jaime Marques <mrthesoulz@gmail.com>
2016-10-03 00:25:34 +0100

    show version

--- interface/mainframe.lua
+++ interface/mainframe.lua
@@ -3,7 +3,7 @@ local logo = '|T'..NeP.Media..'logo.blp:10:10|t'
 NeP.Interface.MainFrame = NeP.Interface:BuildGUI({
 	color = {0,0,0,0.6},
 	size = {100, 0},
-	title = logo..NeP.Color..NeP.Name
+	title = logo..NeP.Color..NeP.Name..' v:'..NeP.Version
 })
 
 local menuFrame = CreateFrame("Frame", "ExampleMenuFrame", NeP.Interface.MainFrame, "UIDropDownMenuTemplate")
