e270bb40b4c11adb57e4416e3d8b3b267b79d511
Jaime Marques <mrthesoulz@gmail.com>
2016-10-03 02:26:45 +0100

    CR stuff

--- System/CRs.lua
+++ System/CRs.lua
@@ -1,16 +1,28 @@
-NeP.CombatRoutines = {
-	CRs = {}
-}
+NeP.CombatRoutines = {}
 
-local CRs = setmetatable({}, { __index = NeP.CombatRoutines.CRs })
+local CRs = {}
+
+--/dump NeP.CombatRoutines:GetCR(NeP.CombatRoutines:GetList()[1])['true'][1][1]
 
 function NeP.CombatRoutines:Add(SpecID, Name, InCombat, OutCombat, ExeOnLoad)
 	local _,_, classIndex = UnitClass('player')
-	if ClassTable[classIndex][SpecID] or ClassTable[SpecID] then
+	if NeP.ClassTable[classIndex][SpecID] or NeP.ClassTable[SpecID] then
 		CRs[Name] = {}
-		CRs[Name].Name = Name
-		CRs[Name][true] = setmetatable({}, { __index = InCombat })
-		CRs[Name][false] = setmetatable({ __index = OutCombat }, {})
-		CRs[Name].Exe = ExeOnLoad
+		CRs[Name]['Name'] = Name
+		CRs[Name]['Exe'] = ExeOnLoad
+		CRs[Name]['true'] = setmetatable({}, {__index=InCombat})
+		CRs[Name]['false'] = setmetatable({}, {__index=OutCombat})
 	end
+end
+
+function NeP.CombatRoutines:GetCR(name)
+	return CRs[name]
+end
+
+function NeP.CombatRoutines:GetList()
+	local result = {}
+	for k,v in pairs(CRs) do
+		result[#result+1] = k
+	end
+	return result
 end
