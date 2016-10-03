798a44db4d634aa035b316a9b4f06ac49080cf47
Jaime Marques <mrthesoulz@gmail.com>
2016-10-03 18:41:15 +0100

    tokenizer

--- System/lexer.lua
+++ System/lexer.lua
@@ -1,7 +1,44 @@
 NeP.Lexer = {}
 
-function NeP.Lexer:Tokenize(eval)
+local tokens = {
+	{ 'string', { '^\'.-\'', '^".-"' } },
+	{ 'logic', { '^and', '^or', '^&&', '^||', '^&', '^|' } },
+	{ 'constant', { '^true', '^false', '^nil', '^null' } },
+	{ 'comparator', { '^>=', '^=>',  '^>', '^<=', '^=<', '^<', '^==', '^!=', '^~=', '^=' } },
+	{ 'not', { '^not', '^!' } },
+	{ 'identifier', {'^[_%a][_%w]*'} },
+	{ 'number', { '^%d+%.?%d*', '^%d+%.?%d*', '^%.%d+' } },
+	{ 'operator', {'^[%*/%-%+%%^]'} },
+	{ 'nest', {'^[{}]'}},
+	{ 'character', {'^[@,:\'%(%)%[%]%.#%$~`\\\";?%s]'} },
+}
+
+local function FindPatern(Strg, token, patterns)
 	
+end
+
+function NeP.Lexer:Tokenize(Strg)
+	local list = {}
+	local index = 1
+	local length = #Strg + 1
+	while index < length do
+		for i = 1, #tokens do
+			local token, patterns = tokens[i][1], tokens[i][2]
+			for i = 1, #patterns do
+				local sI, eI = Strg:find(patterns[i], index)
+				if sI then
+					index = eI + 1
+					list[#list+1] = {
+						kind = token,
+						value = Strg:sub(sI, eI),
+						from = sI,
+						to = eI
+					}
+				end
+			end
+		end
+	end
+	return list
 end
 
 function NeP.Lexer:STRING(eval)
