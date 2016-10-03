5ce29aab14040de4024a8d8972c0c4dd55796e5e
Jaime Marques <mrthesoulz@gmail.com>
2016-10-03 03:15:56 +0100

    cr stuff

--- System/CRs.lua
+++ System/CRs.lua
@@ -1,28 +1,42 @@
-NeP.CombatRoutines = {}
+NeP.CombatRoutines = {
+	CRs = {}
+}
 
-local CRs = {}
-
---/dump NeP.CombatRoutines:GetCR(NeP.CombatRoutines:GetList()[1])['true'][1][1]
+local smt = setmetatable
+local UnitClass = UnitClass
+local CRs = smt({}, {__index=NeP.CombatRoutines.CRs})
 
 function NeP.CombatRoutines:Add(SpecID, Name, InCombat, OutCombat, ExeOnLoad)
-	local _,_, classIndex = UnitClass('player')
-	if NeP.ClassTable[classIndex][SpecID] or NeP.ClassTable[SpecID] then
-		CRs[Name] = {}
-		CRs[Name]['Name'] = Name
-		CRs[Name]['Exe'] = ExeOnLoad
-		CRs[Name]['true'] = setmetatable({}, {__index=InCombat})
-		CRs[Name]['false'] = setmetatable({}, {__index=OutCombat})
+	local classIndex = select(3, UnitClass('player'))
+	if NeP.ClassTable[classIndex][SpecID] or classIndex == SpecID then
+		if not CRs[SpecID] then
+			CRs[SpecID] = {}
+		end
+		CRs[SpecID][Name] = {}
+		CRs[SpecID][Name]['Exe'] = ExeOnLoad
+		CRs[SpecID][Name]['true'] = smt({}, {__index=InCombat})
+		CRs[SpecID][Name]['false'] = smt({}, {__index=OutCombat})
 	end
 end
 
-function NeP.CombatRoutines:GetCR(name)
-	return CRs[name]
+function NeP.CombatRoutines:GetCR(SpecID, Name)
+	return CRs[SpecID] and CRs[SpecID][Name]
 end
 
 function NeP.CombatRoutines:GetList()
 	local result = {}
-	for k,v in pairs(CRs) do
-		result[#result+1] = k
+	local Spec = GetSpecializationInfo(GetSpecialization())
+	local Class = select(3, UnitClass('player'))
+	if CRs[Spec] then
+		for k,v in pairs(CRs[Spec]) do
+			print(k)
+			result[#result+1] = k
+		end
+	end
+	if CRs[Class] then
+		for k,v in pairs(CRs[Class]) do
+			result[#result+1] = k
+		end
 	end
 	return result
 end
